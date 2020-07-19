259dfb14c194d864433da11b4c617d349775c565
andrenba <andrenba@gmail.com>
2020-07-18 23:28:40 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 function httpGet(theUrl)
 {
     var xmlHttp = new XMLHttpRequest();
-    xmlHttp.open( "GET", theUrl);
-    xmlHttp.send();
+    xmlHttp.open( "GET", theUrl, false);
+    xmlHttp.send(null);
     bla=JSON.parse(xmlHttp.responseText);
     return bla;
 }
