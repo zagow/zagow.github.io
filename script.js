fd449b413b9bd9e02c8c85e3639c4a85204359b3
andrenba <andrenba@gmail.com>
2020-07-18 23:06:34 -0300

    We are here!

---
+++ script.js
@@ -0,0 +1,10 @@
+function httpGet(theUrl)
+{
+    var xmlHttp = new XMLHttpRequest();
+    xmlHttp.open( "GET", theUrl, false );
+    xmlHttp.send( null );
+    bla=JSON.parse(xmlHttp.responseText);
+    return bla;
+}
+bla=httpGet('https://www.affirmations.dev/');
+document.getElementById('quoteoftheday').innerHTML=bla.affirmation;
