a6548479143bee6b7dfa64663bfe686ae7549392
andrenba <andrenba@gmail.com>
2020-07-22 01:05:39 -0300

    update apoie.js

--- apoie.js
+++ apoie.js
@@ -1,4 +1,4 @@
 apoieOsite = document.createElement('div');
 apoieOsite.setAttribute('id', 'apoieOsite');
-apoieOsite.style="position: -webkit-sticky;position: sticky;top: 0; width: 100%; background-color:red;";
+apoieOsite.style="position:-webkit-sticky;position: sticky;top: 0; width: 100%; background-color:red;";
 document.body.appendChild(apoieOsite);
