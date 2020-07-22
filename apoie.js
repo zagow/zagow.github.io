fed4f831fdb1ed7afe586279fb2abad23a41fb41
andrenba <andrenba@gmail.com>
2020-07-22 01:21:32 -0300

    update apoie.js

--- apoie.js
+++ apoie.js
@@ -2,6 +2,6 @@ function bla(){
   
   apoieOsite = document.createElement('div');
 apoieOsite.setAttribute('id', 'apoieOsite');
-apoieOsite.style="position:-webkit-sticky;position: sticky;top: 0; width: 100%; background-color:red;";
+apoieOsite.style="position: sticky;top: 0; width: 100%; background-color:red;";
 document.body.appendChild(apoieOsite);
 }
